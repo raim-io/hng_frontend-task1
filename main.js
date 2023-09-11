bcf09752a3aa297815e772c80235d9284377f699
raim-io <raim.tobi@gmail.com>
2023-09-11 07:05:01 +0100

    implemented real-time  utc time update

--- main.js
+++ main.js
@@ -6,12 +6,36 @@ import './style.css'
 //  </p>
 //`
 
-const currentDay = document.querySelector('#current-day');
-const currentTime = document.querySelector('#current-time');
+//
+//
 
-const date = new Date();
-const dayOfWeek = date.toLocaleDateString('en-US', { weekday: 'long' }); 
-const utcTime = date.toISOString().slice(0, -5) + 'Z';
+//const date = new Date();
+//const dayOfWeek = date.toLocaleDateString('en-US', { weekday: 'long' }); 
+//const utcTime = date.toISOString().slice(0, -5) + 'Z';
 
-currentDay.textContent = dayOfWeek;
-currentTime.textContent = utcTime;
+//currentDay.textContent = dayOfWeek;
+//currentTime.textContent = utcTime;
+
+document.addEventListener("DOMContentLoaded", function () {
+	const currentDay = document.querySelector('[data-testid="currentDayOfTheWeek"]');
+  const currentTime = document.querySelector('[data-testid="currentUTCTime"]');
+	
+	currentDay.textContent = getCurrentDate();
+	currentTime.textContent;
+
+    // Update current time every second
+    setInterval(function () {
+        getCurrentTime(currentTime);
+    }, 1000);
+});
+
+function getCurrentDate() {
+	const date = new Date();
+	const dayOfWeek = date.toLocaleDateString('en-US', { weekday: 'long' });
+	return dayOfWeek;
+}
+
+function getCurrentTime(element) {
+    const utcTime = Date.now();
+    element.textContent = utcTime;
+}
